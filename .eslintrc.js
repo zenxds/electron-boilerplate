38d20a67f3c7fc629bdd5296e2141f46660300b0
zenxds <zenxds@gmail.com>
2022-01-18 17:51:01 +0800

    feat: 优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,22 +12,21 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['react', '@typescript-eslint'],
   globals: {
     nodeRequire: true,
     API_SERVER_PLACEHOLDER: true,
     __webpack_public_path__: true,
   },
   rules: {
-    '@typescript-eslint/indent': ['warn', 2],
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
+    ],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,22 +1,14 @@
 import React from 'react'
-import JSONStore from '@utils/JSONStore'
+import DataStore from '@utils/DataStore'
 
 declare global {
   type IReactComponent<P = any> = React.ComponentClass<P> | React.ClassicComponentClass<P>
-
-  interface LooseObject {
-    [key: string]: any
-  }
-
-  interface Dictionary<T> {
-    [key: string]: T
-  }
 
   interface GenericFunc<T> {
     (...args: any[]): T
   }
 
   interface CommonProps {
-    dataStore: JSONStore
+    dataStore: DataStore
   }
 }
